eaa4f1da4f4fe2e84dba84f6706b24cbe0990944
kodirzaelani <laman.kreasi@gmail.com>
2023-05-18 15:50:29 +0800

    Hode Mode Debug

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'API Notes',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Buku Catatan'),
     );
   }
 }
